a75a37f2551417177a28694544d32c76347a47f5
Harish-85 <66102112+Harish-85@users.noreply.github.com>
2023-06-29 17:31:41 +0530

    Minor bug fixes and camera smoothing

---
+++ Assets/Scripts/DeathVoid.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathVoid : MonoBehaviour
+{
+
+    [SerializeField] private int rayCount;
+
+    [SerializeField]private float width;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CastRays();
+    }
+
+    bool CastRays()
+    {
+        //cast a ray from the left to right pointing to the top
+        //if any of the rays hit the player, return true
+        //else return false
+        
+        //get the direction of the ray
+        var direction = Vector2.up;
+        //get the start position of the ray
+        for( int i = 0; i < rayCount; i++)
+        {
+            var startPos = transform.position + Vector3.left * width / 2 + Vector3.right * width / (rayCount - 1) * i;
+            var hit = Physics2D.Raycast(startPos, direction);
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("Player hit");
+                    return true;
+                }
+            }
+            
+            Debug.DrawRay(startPos, direction, Color.red);
+        }
+
+        return false;
+    }
+}
