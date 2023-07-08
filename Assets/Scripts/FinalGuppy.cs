8dd8c100ea48e8f793a8dedd1e92e363fea1fc71
Harish-85 <66102112+Harish-85@users.noreply.github.com>
2023-07-08 19:24:39 +0530

    Too many gupps fixed

--- Assets/Scripts/FinalGuppy.cs
+++ Assets/Scripts/FinalGuppy.cs
@@ -9,28 +9,37 @@ public class FinalGuppy : MonoBehaviour
     [SerializeField] private Vector2 bounds;
     [SerializeField] private float speed = 1;
     [SerializeField] private float timeBetMove = 1;
+    [SerializeField] private List<LTDescr> tweenDescrs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeanTween.init(1000);
+        tweenDescrs = new List<LTDescr>();
     }
 
     private float timePassed = 0;
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(LeanTween.tweensRunning);
        // only run this code once every 5 seconds
         timePassed += Time.deltaTime;
         if (timePassed > timeBetMove)
         {
             timePassed = 0;
+
+            foreach (var descr in tweenDescrs)
+            {
+                LeanTween.cancel(descr.id);
+            }
+            tweenDescrs.Clear();
+
             foreach (var g in Guppys)
             {
-                //pick a random position within the bounds and move the guppy to that position using LeanTween ONLY CHANGE LOCAL POSITION
+                
                 Vector3 randomPos = new Vector3(Random.Range(-bounds.x, bounds.x), Random.Range(-bounds.y, bounds.y));
-                LeanTween.moveLocal(g.gameObject, randomPos, speed);
-
-
+                tweenDescrs.Add(LeanTween.moveLocal(g.gameObject, randomPos, speed));
             }
             
         }
